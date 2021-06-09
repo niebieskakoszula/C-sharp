16e4037f8b129ef0ae1ff28308c0c137125af082
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-09 15:27:53 +0200

    Create Program.cs

---
+++ kolka_i_kropki/Program.cs
@@ -0,0 +1,11 @@
+namespace Jipp_4_Projekt_Uproszczony_v2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rozgrywka r = new Rozgrywka();
+            r.PrzygotujGre();
+        }
+    }
+}
