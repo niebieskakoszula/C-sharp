06e2c35cdc464b9f46364d01706bad453a929976
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-09 15:28:36 +0200

    Add files via upload

---
+++ kolka_i_kropki/Punkt.cs
@@ -0,0 +1,187 @@
+﻿namespace Jipp_4_Projekt_Uproszczony_v2_2
+{
+    abstract class PunktAbstrakcyjny
+    {
+
+        protected PunktAbstrakcyjny gora;
+        protected PunktAbstrakcyjny dol;
+        protected PunktAbstrakcyjny prawo;
+        protected PunktAbstrakcyjny lewo;
+        public abstract PunktAbstrakcyjny Gora { get; }
+        public abstract PunktAbstrakcyjny Prawo { get; }
+        public abstract PunktAbstrakcyjny Dol { get; }
+        public abstract PunktAbstrakcyjny Lewo { get; }
+        protected int koniec;
+        protected int odleglosc;
+        public abstract int Odleglosc { get; set; }
+        public int Koniec
+        {
+            get => koniec;
+            set
+            {
+                if (value == 1 || value == 2) koniec = value;
+            }
+        }
+        public virtual void PrzypiszSasiadow(PunktAbstrakcyjny g, PunktAbstrakcyjny p, PunktAbstrakcyjny d, PunktAbstrakcyjny l) { }
+        public abstract void PolaczKonce(int i);
+        public abstract bool CzyPolaczenia();
+        public abstract int PokazPolaczenie(int kierunek);
+
+        public abstract int[] PokazRoznice();
+    }
+    class Punkt : PunktAbstrakcyjny
+    {
+        public Punkt(int odleglosc) { this.odleglosc = odleglosc; koniec = 0;}
+        public Punkt(int odleglosc, int koniec) { this.odleglosc = odleglosc; this.koniec = koniec;}
+        protected int[] polaczenia = new int[4];
+        public override int Odleglosc
+        {
+            get => odleglosc;
+            set
+            {
+                if (value >= 0) odleglosc = value;
+            }
+        }
+
+
+        public override PunktAbstrakcyjny Gora => gora;
+        public override PunktAbstrakcyjny Prawo => prawo;
+        public override PunktAbstrakcyjny Dol => dol;
+        public override PunktAbstrakcyjny Lewo => lewo;
+        public override void PrzypiszSasiadow(PunktAbstrakcyjny g, PunktAbstrakcyjny p, PunktAbstrakcyjny d, PunktAbstrakcyjny l)
+        {
+            if (gora != null) return;
+            gora = g; if (gora is Punkt) polaczenia[0] = 1; else polaczenia[0] = -1;
+            dol = d; if (dol is Punkt) polaczenia[2] = 1; else polaczenia[2] = -1;
+            if (koniec != 0)
+            {
+                prawo = p; polaczenia[1] = -1;
+                lewo = l; polaczenia[3] = -1;
+            }
+            else
+            {
+                prawo = p; if (prawo is Punkt) polaczenia[1] = 1; else polaczenia[1] = -1;
+                lewo = l; if (lewo is Punkt) polaczenia[3] = 1; else polaczenia[3] = -1;
+            }
+
+        }
+
+        public void Polacz(int kierunek)
+        {
+            if (polaczenia[kierunek - 1] != 1) return;
+            polaczenia[kierunek - 1] = 0;
+            switch (kierunek)
+            {
+                case 1: gora.PolaczKonce(koniec); break;
+                case 2: prawo.PolaczKonce(koniec); break;
+                case 3: dol.PolaczKonce(koniec); break;
+                case 4: lewo.PolaczKonce(koniec); break;
+            }
+        }
+        public virtual void Zablokuj(Blokada b, int kierunek)
+        {
+            if (polaczenia[kierunek - 1] != 1) return;
+            switch (kierunek)
+            {
+                case 1: gora = b; break;
+                case 2: prawo = b; break;
+                case 3: dol = b; break;
+                case 4: lewo = b; break;
+            }
+            polaczenia[kierunek - 1] = -1;
+        }
+        public override void PolaczKonce(int i)
+        {
+            if (i == 0 || koniec == i) return;
+            else if (koniec != 0 && koniec != i) koniec = 3;
+            else koniec = i;
+
+            if (polaczenia[0] == 0) gora.PolaczKonce(koniec);
+            if (polaczenia[1] == 0) prawo.PolaczKonce(koniec);
+            if (polaczenia[2] == 0) dol.PolaczKonce(koniec);
+            if (polaczenia[3] == 0) lewo.PolaczKonce(koniec);
+        }
+        public override int PokazPolaczenie(int kierunek)
+        {
+            return polaczenia[kierunek - 1];
+        }
+        public override bool CzyPolaczenia()
+        {
+            for (int i = 0; i < 4; i++)
+                if (polaczenia[i] == 0)
+                    return true;
+
+            return false;
+        }
+
+
+        public void PolaczTymczasowo(int kierunek)
+        {
+            if (polaczenia[kierunek - 1] != 1) return;
+            polaczenia[kierunek - 1] = 0;
+        }
+        public void ZablokujTymczasowo(int kierunek)
+        {
+            polaczenia[kierunek - 1] = -1;
+        }
+        public void Odblokuj(int kierunek)
+        {
+            switch (kierunek)
+            {
+                case 1: if(gora is Blokada) return; break;
+                case 2: if(prawo is Blokada) return; break;
+                case 3: if(dol is Blokada) return; break;
+                case 4: if(lewo is Blokada) return; break;
+            }
+            polaczenia[kierunek - 1] = 1;
+        }
+
+
+        public override int[] PokazRoznice()
+        {
+            int[] wynik = new int[5];
+            if (odleglosc == 0) wynik[4] = 1;
+
+            PunktAbstrakcyjny sasiad = null;
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0: sasiad = gora; break;
+                    case 1: sasiad = prawo; break;
+                    case 2: sasiad = dol; break;
+                    case 3: sasiad = lewo; break;
+                }
+
+                if(polaczenia[i] == 0)
+                {
+                    if (sasiad.Odleglosc == odleglosc) wynik[i] = 0;
+                    else wynik[i] = 3;
+                }
+                else if(polaczenia[i] == 1)
+                {
+                    if(sasiad.Odleglosc > odleglosc + 1) wynik[i] = 3;
+                    else if (sasiad.Odleglosc == odleglosc + 1) wynik[i] = 2;
+                    else if (sasiad.Odleglosc == odleglosc) wynik[i] = 1;
+                    else { wynik[i] = -1; wynik[4] = 1; }
+                }
+                else wynik[i] = -2;
+            }
+
+            return wynik;
+        }
+    }
+    class Blokada : PunktAbstrakcyjny
+    {
+        public override PunktAbstrakcyjny Gora => this;
+        public override PunktAbstrakcyjny Prawo => this;
+        public override PunktAbstrakcyjny Dol => this;
+        public override PunktAbstrakcyjny Lewo => this;
+        public override int Odleglosc { get => -1; set { } }
+        public override void PolaczKonce(int i) { return; }
+        public override bool CzyPolaczenia() { return false; }
+        public override int PokazPolaczenie(int kierunek) { return -1; }
+
+        public override int[] PokazRoznice() { return new int[0]; }
+    }
+}
---
+++ kolka_i_kropki/Wyswietlanie.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jipp_4_Projekt_Uproszczony_v2_2
+{
+    class Wyswietlanie
+    {
+        int n;
+        string[,] znaki;
+        ConsoleColor[,] kolory;
+        ConsoleColor pierwszy;
+        ConsoleColor drugi;
+        public ConsoleColor Pierwszy
+        {
+            get => pierwszy;
+            set
+            {
+                if (value != drugi) pierwszy = value;
+                else Console.WriteLine("kolor już zajęty");
+            }
+        }
+        public ConsoleColor Drugi
+        {
+            get => drugi;
+            set
+            {
+                if (value != pierwszy) drugi = value;
+                else Console.WriteLine("kolor już zajęty");
+            }
+        }
+        public Wyswietlanie(int nn, ConsoleColor p, ConsoleColor d)
+        {
+            pierwszy = p;
+            drugi = d;
+            n = nn;
+            znaki = new string[(n * 2) - 1, (n * 2) - 1];
+            kolory = new ConsoleColor[(n * 2) - 1, (n * 2) - 1];
+
+            for (int i = 0; i < (n * 2) - 2; i += 2)
+            {
+                for (int j = 0; j < (n * 2) - 2; j += 2)
+                {
+                    znaki[i, j + 1] = Convert.ToString((i / 2) + 1); kolory[i, j + 1] = pierwszy;
+                    znaki[i, j] = " "; kolory[i, j] = ConsoleColor.Black;
+                    znaki[i + 1, j] = Convert.ToString((j / 2) + 1); kolory[i + 1, j] = drugi;
+                    znaki[i + 1, j + 1] = " "; kolory[i + 1, j + 1] = ConsoleColor.Black;
+                }
+                znaki[i, (n * 2) - 2] = " "; kolory[i, (n * 2) - 2] = ConsoleColor.Black;
+                znaki[i + 1, (n * 2) - 2] = Convert.ToString(n); kolory[i + 1, (n * 2) - 2] = drugi;
+            }
+            for (int j = 0; j < (n * 2) - 2; j += 2)
+            {
+                znaki[(n * 2) - 2, j + 1] = Convert.ToString(n); kolory[(n * 2) - 2, j + 1] = pierwszy;
+                znaki[(n * 2) - 2, j] = " "; kolory[(n * 2) - 2, j] = ConsoleColor.Black;
+            }
+            znaki[(n * 2) - 2, (n * 2) - 2] = " "; kolory[(n * 2) - 2, (n * 2) - 2] = ConsoleColor.Black;
+        }
+        ConsoleColor WybierzKolor()
+        {
+            Console.WriteLine("Wybierz swój kolor:");
+            Console.WriteLine("1. Niebieski");
+            Console.WriteLine("2. Ciemny niebieski");
+            Console.WriteLine("3. Żółty");
+            Console.WriteLine("4. Ciemny żółty");
+            Console.WriteLine("5. Czerwony");
+            Console.WriteLine("6. Ciemny czerwony");
+            Console.WriteLine("7. Zielony");
+            Console.WriteLine("8. Ciemny zielony");
+            Console.WriteLine("9. Cyjan");
+            Console.WriteLine("10. Ciemny cyjan");
+            Console.WriteLine("11. Magenta");
+            Console.WriteLine("12. Ciemna magenta");
+            Console.WriteLine("W innym wypadku wybrany zostanie losowy kolor");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1: return ConsoleColor.Blue;
+                case 2: return ConsoleColor.DarkBlue;
+                case 3: return ConsoleColor.Yellow;
+                case 4: return ConsoleColor.DarkYellow;
+                case 5: return ConsoleColor.Red;
+                case 6: return ConsoleColor.DarkRed;
+                case 7: return ConsoleColor.Green;
+                case 8: return ConsoleColor.DarkGreen;
+                case 9: return ConsoleColor.Cyan;
+                case 10: return ConsoleColor.DarkCyan;
+                case 11: return ConsoleColor.Magenta;
+                case 12: return ConsoleColor.DarkMagenta;
+                default:
+                    var kolory = Enum.GetValues(typeof(ConsoleColor));
+                    ConsoleColor kolor;
+                    do
+                    {
+                        kolor = (ConsoleColor)kolory.GetValue((new Random()).Next(kolory.Length));
+                    } while (kolor == ConsoleColor.White);
+                    return kolor;
+            }
+        }
+
+        public void ZaznaczWygrana(bool gracz1, int[] ruch)
+        {
+            int x, y;
+            if (gracz1) { x = (ruch[1] - 1) * 2; y = ((ruch[0] - 1) * 2) + 1; }
+            else { x = ((ruch[0] - 1) * 2) + 1; y = ((ruch[1] - 1) * 2); }
+
+            Queue<int> koordynaty = new Queue<int>();
+            koordynaty.Enqueue(x); koordynaty.Enqueue(y);
+            kolory[x, y] = ConsoleColor.White;
+
+            while (koordynaty.Count != 0)
+            {
+                x = koordynaty.Peek(); koordynaty.Dequeue();
+                y = koordynaty.Peek(); koordynaty.Dequeue();
+
+                if (y - 1 >= 0 && znaki[x, y - 1] == "-" && kolory[x, y - 1] != kolory[x, y])
+                {
+                    kolory[x, y - 1] = ConsoleColor.White;
+                    kolory[x, y - 2] = ConsoleColor.White;
+                    koordynaty.Enqueue(x); koordynaty.Enqueue(y - 2);
+                }
+                if (x + 1 <= (n * 2) - 2 && znaki[x + 1, y] == "|" && kolory[x + 1, y] != kolory[x, y])
+                {
+                    kolory[x + 1, y] = ConsoleColor.White;
+                    kolory[x + 2, y] = ConsoleColor.White;
+                    koordynaty.Enqueue(x + 2); koordynaty.Enqueue(y);
+                }
+                if (y + 1 <= (n * 2) - 2 && znaki[x, y + 1] == "-" && kolory[x, y + 1] != kolory[x, y])
+                {
+                    kolory[x, y + 1] = ConsoleColor.White;
+                    kolory[x, y + 2] = ConsoleColor.White;
+                    koordynaty.Enqueue(x); koordynaty.Enqueue(y + 2);
+                }
+                if (x - 1 >= 0 && znaki[x - 1, y] == "|" && kolory[x - 1, y] != kolory[x, y])
+                {
+                    kolory[x - 1, y] = ConsoleColor.White;
+                    kolory[x - 2, y] = ConsoleColor.White;
+                    koordynaty.Enqueue(x - 2); koordynaty.Enqueue(y);
+                }
+            }
+        }
+        public void Wyswietl()
+        {
+            Console.Clear();
+            Console.Write("    ");
+            Console.ForegroundColor = pierwszy;
+            for (int j = 0; j < n - 1; j++) Console.Write(j + 1 + " ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < (n * 2) - 1; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.ForegroundColor = drugi;
+                    Console.Write((i / 2) + 1);
+                }
+                else Console.Write(" ");
+
+                Console.Write("  ");
+                for (int j = 0; j < (n * 2) - 1; j++)
+                {
+                    Console.ForegroundColor = kolory[i, j];
+                    Console.Write(znaki[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void PoprawWyswietlanie(bool gracz1, int[] ruch)
+        {
+            if (gracz1)
+            {
+                if (ruch[0] == ruch[2]) { znaki[((ruch[1] - 1) * 2) + 1, ((ruch[0] - 1) * 2) + 1] = "|"; kolory[((ruch[1] - 1) * 2) + 1, ((ruch[0] - 1) * 2) + 1] = pierwszy; }  
+                else { znaki[(ruch[1] - 1) * 2, ruch[0] * 2] = "-"; kolory[(ruch[1] - 1) * 2, ruch[0] * 2] = pierwszy; }
+            }
+            else
+            {
+                if (ruch[0] == ruch[2]) { znaki[((ruch[0] - 1) * 2) + 1, ((ruch[1] - 1) * 2) + 1] = "-"; kolory[((ruch[0] - 1) * 2) + 1, ((ruch[1] - 1) * 2) + 1] = drugi; }
+                else { znaki[ruch[0] * 2, ((ruch[1] - 1) * 2)] = "|"; kolory[ruch[0] * 2, ((ruch[1] - 1) * 2)] = drugi; }
+            }
+        }
+    }
+}
