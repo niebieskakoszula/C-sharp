424ce02e2722c5233ca818164fbe653bbf8df3cd
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-17 00:35:36 +0200

    Update Dzialania_Na_Ulamkach.cs

--- Dzialania_Na_Ulamkach.cs
+++ Dzialania_Na_Ulamkach.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+////////////////////////////
+////wersja rekurencyjna/////
+////////////////////////////
+
 namespace Dzialania_Na_Ulamkach
 {
     class Ulamek
