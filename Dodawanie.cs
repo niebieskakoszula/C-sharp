7a400b8efc37def7a80933ca10ddd83ddf286008
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-24 15:55:05 +0200

    Update Dodawanie.cs

--- Dodawanie.cs
+++ Dodawanie.cs
@@ -1,7 +1,6 @@
 //https://pl.spoj.com/problems/JSUMDUZE/
 
 using System;
-using System.Text;
 using System.Numerics;
 
 namespace JSUMDUZE___Dodawanie
@@ -10,25 +9,20 @@ namespace JSUMDUZE___Dodawanie
     {
         static void Main(string[] args)
         {
-            BigInteger pierwsza;
-            BigInteger druga;
-            BigInteger wynik;
-            int t;
-            string wczytywanie;
-            string[] temp = new string[2];
-            t = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < t; i++)
+            BigInteger first;
+            BigInteger second;
+            BigInteger result;
+            string[] input;
+            int tests = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
             {
-                pierwsza = new BigInteger();
-                druga = new BigInteger();
-                wynik = new BigInteger();
-                wczytywanie = Console.ReadLine();
-                temp = wczytywanie.Split(' ');
-                pierwsza = BigInteger.Parse(temp[0]);
-                druga = BigInteger.Parse(temp[1]);
-                wynik = BigInteger.Add(pierwsza, druga);
-                Console.WriteLine(wynik);
+                input = Console.ReadLine().Split(' ');
+                first = BigInteger.Parse(input[0]);
+                second = BigInteger.Parse(input[1]);
+                result = BigInteger.Add(first, second);
+                Console.WriteLine(result);
             }
         }
     }
 }
+
