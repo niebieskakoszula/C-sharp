cda12a11343fbb48107d596b083809a44b82262d
niebieskakoszula <62652473+niebieskakoszula@users.noreply.github.com>
2021-06-25 15:15:08 +0200

    Create Program.cs

---
+++ Gra Shannona (Uproszczona)/Program.cs
@@ -0,0 +1,12 @@
+namespace Jipp_4_Projekt_Uproszczony_v2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game r = new Game();
+            r.PrepareGame();
+            r.Play();
+        }
+    }
+}
